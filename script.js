e9c263704792aa4facd3483d01c14171d8b42a98
DanielPawelko <daniel.pawelko@ocsbstudent.ca>
2021-01-04 15:36:26 +0000

    done?

--- script.js
+++ script.js
@@ -2,19 +2,16 @@ document.getElementById('button-id').addEventListener('click', checkMovie)
 const output = document.getElementById('output-id')
 function checkMovie () {
   const age = document.getElementById('input-id').value
-  if (age >= 17){
+  if (age >= 17) {
     console.log('17 or older')
     output.innerHTML = 'Available movies depending on your age : R rated, PG 13, and G rated'
-  }
-  else if (age >= 13){
+  } else if (age >= 13) {
     console.log('13 or older')
     output.innerHTML = 'Available movies depending on your age : PG 13 and G rated'
-  }
-  else if (isNaN(age)){
-    console.log('Please input valid intiger')
+  } else if (isNaN(age) || age < 0) {
+    console.log('Please input valid positive intiger')
     output.innerHTML = 'Please input valid intiger'
-  }
-  else {
+  } else {
     console.log('G rated')
     output.innerHTML = 'Available movies depending on your age : G rated'
   }
